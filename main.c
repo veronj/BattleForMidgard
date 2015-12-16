e7fef160d65d0c2580f5f42ce31fbead9cf336c1
Jb-Veron <jb-veron@Jb-debian>
2015-12-16 11:15:17 +0100

    envoi main

--- main.c
+++ main.c
@@ -5,12 +5,13 @@
 ** Login   <veron_j@etna-alternance.net>
 ** 
 ** Started on  Wed Dec 16 09:27:18 2015 VERON Jean-baptiste
-** Last update Wed Dec 16 10:33:39 2015 VERON Jean-baptiste
+** Last update Wed Dec 16 10:47:47 2015 VERON Jean-baptiste
 */
 
 #include <unistd.h>
 #include <stdio.h>
 #include "my_lib.h"
+#include "functions.h"
 
 int	main(int argc, char **argv)
 {
@@ -21,6 +22,7 @@ int	main(int argc, char **argv)
 	  my_putstr("Votre aventure commence ");
 	  my_putstr(argv[2]);
 	  my_putstr("\n");
+	  start(argv[2]);
 	}
       else
 	my_putstr("Usage : ./sta -n your_name\n");

26c8d6129a38da82ca192cae98ce79d901677fa0
Jb-Veron <jb-veron@Jb-debian>
2015-12-16 11:00:36 +0100

    envoi start

---
+++ start.c
@@ -0,0 +1,17 @@
+/*
+** start.c for  in /home/jb-veron/C/Midgard
+** 
+** Made by VERON Jean-baptiste
+** Login   <veron_j@etna-alternance.net>
+** 
+** Started on  Wed Dec 16 10:50:58 2015 VERON Jean-baptiste
+** Last update Wed Dec 16 10:56:27 2015 VERON Jean-baptiste
+*/
+
+#include "my_lib.h"
+
+int	start(char *name)
+{
+  my_putstr("Hello");
+  my_putstr(name);
+}
